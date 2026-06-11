9459a0a97c0d2ef8b1f66662c7c035403836a1ef
andres <anfetv.10@gmail.com>
2026-06-11 08:02:00 -0500

    ajustes layout 1

--- app/page.js
+++ app/page.js
@@ -1,17 +1,1 @@
-import { redirect } from 'next/navigation';
-import { getSession } from '@/lib/auth';
-
-export const dynamic = 'force-dynamic';
-
-export default async function Home() {
-  try {
-    const session = await getSession();
-    if (session) {
-      redirect('/dashboard');
-    } else {
-      redirect('/login');
-    }
-  } catch {
-    redirect('/login');
-  }
-}
+export { default } from './(auth)/login/page';
